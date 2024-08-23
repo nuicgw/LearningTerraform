3ddc1a7ca017f48107ba8cd5875df3f1d57511a5
Aaron Price <hello-aaronprice@outlook.com>
2024-08-23 18:49:56 +0800

    Added random_pet

--- 01-terraform-basics/01-localFile.tf
+++ 01-terraform-basics/01-localFile.tf
@@ -1,4 +1,10 @@
 resource "local_file" "first-local-file" {
     filename = var.filename
-    content = var.file_content
+    content = "Random Pet name ${random_pet.aarons-pet.id}"
+}
+
+resource "random_pet" "aarons-pet" {
+    length = 8
+    prefix = "Mr"
+    separator = "."
 }
